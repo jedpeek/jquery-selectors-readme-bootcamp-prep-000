99fbb3739ba1483bbcd47f621439b369f58bfcd4
Jed Peek <jedpeek@yahoo.com>
2018-05-02 15:58:32 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,13 +1,23 @@
 // declare your functions here...
 $(document).ready(function(){
 function paragraphSelector(){
-  return $('p')
+  return $('p');
 }
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+
 $('img');
 $('.pics');
-$('#baby-ninja').append("Banana");
+$('#baby-ninja')
 $('ul li');
 $('div img:first-child');
 $('img[alt="the beatles making faces"]');
-$('div:last');
+$('img:last');
 });
