a2e7c79943275c326d56cd0e14438ee2c38a7a38
Jed Peek <jedpeek@yahoo.com>
2018-05-02 15:49:47 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,7 +1,7 @@
 // declare your functions here...
 $('img');
 $('.pics');
-$('#baby-ninja');
+$('#baby-ninja').append("Banana");
 $('ul li');
 $('div img:first-child');
 $('img[alt="the beatles making faces"]');
